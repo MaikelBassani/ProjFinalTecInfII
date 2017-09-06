d6ac5f752b3252839fbfc6ed85431c2447e41df5
MaikelBassani <maikel.bassani@catolicasc.org.br>
2017-09-05 21:55:41 -0300

    fazendo o trabalho

---
+++ Vidly/Controllers/PetsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewsModels;
+
+namespace Vidly.Controllers
+{
+    public class PetsController : Controller
+    {
+        public IEnumerable<Pet> GetPets()
+        {
+            return new List<Pet>
+            {
+                new Pet { nome = "Yorkshire", id = 1},
+                new Pet {nome = "Pastor Alemão", id = 2}
+            };
+        }
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var pets = new Pet() { nome = "Nada!" };
+
+            var customers = new List<Customer>
+            {
+                new Customer {nome = "Customer 1"},
+                new Customer {nome = "Customer 2"}
+            };
+
+            var viewModel = new RandomPetsViewModel
+            {
+                Pets = pets,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Index()
+        {
+            var animal = GetPets();
+
+            return View(animal);
+        }
+    }
+}
