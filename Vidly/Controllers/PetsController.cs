d8152b26f36bd06a1a850478195816dbc9faabb3
MaikelBassani <maikel.bassani@catolicasc.org.br>
2017-10-03 20:10:37 -0300

    Arquivos certos

---
+++ Vidly/Controllers/PetsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class PetsController : Controller
+    {
+        private ApplicationDbContext _context;
+        public PetsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        public ActionResult Index()
+        {
+            var pets = _context.Pet.ToList();
+            return View(pets);
+        }
+        public ActionResult Details(int Id)
+        {
+            var pets = _context.Pet.SingleOrDefault(c => c.id == Id);
+
+
+            if (pets == null)
+                return HttpNotFound();
+
+            return View(pets);
+        }
+
+        
+    }
+}
---
+++ Vidly/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Customer
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+    }
+
+    
+}
