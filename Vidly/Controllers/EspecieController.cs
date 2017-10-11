1812d179db9187570a8c61a66a20af98b5adec82
MaikelBassani <maikel.bassani@catolicasc.org.br>
2017-10-10 21:03:45 -0300

    Fazendo o botão edição

--- Vidly/Controllers/EspecieController.cs
+++ Vidly/Controllers/EspecieController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewsModels;
 
 namespace Vidly.Controllers
 {
@@ -34,6 +35,49 @@ namespace Vidly.Controllers
                 return HttpNotFound();
 
             return View(especies);
+        }
+        public ActionResult Edit(int id)
+        {
+            var especie = _context.Especie.SingleOrDefault(c => c.id == id);
+
+            if (especie == null)
+                return HttpNotFound();
+
+            var viewModel = new EspecieFormViewModels
+            {
+                Especie = especie
+
+            };
+
+            return View("EspecieForm", viewModel);
+        }
+        [HttpPost] // só será acessada com POST
+        public ActionResult Save(Species especie)
+        {
+            if (especie.id == 0)
+            {
+                _context.Especie.Add(especie);
+            }
+            else
+            {
+                var especieInDb = _context.Especie.Single(c => c.id == especie.id);
+
+                especieInDb.nome = especie.nome;
+            }
+
+            // faz a persistência
+            _context.SaveChanges();
+            // Voltamos para a lista de clientes
+            return RedirectToAction("Index");
+        }
+        public ActionResult New()
+        {
+            var viewModel = new EspecieFormViewModels
+            {
+                
+            };
+
+            return View("EspecieForm", viewModel);
         }
 	}
 }
---
+++ Vidly/ViewsModels/EspecieFormViewModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewsModels
+{
+    public class EspecieFormViewModels
+    {
+        public  Species Especie { get; set; }
+        public string Title
+        {
+            get
+            {
+                if (Especie != null && Especie.id != 0)
+                    return "Editar especie";
+
+                return "Nova especie";
+            }
+        }
+    }
+}
